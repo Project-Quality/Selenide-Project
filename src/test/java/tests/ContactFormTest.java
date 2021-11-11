eec903bbf4eec42a2f940d2f38b24e57a9ddce09
Krzysztof Raczyński <krzysztof@projectquality.it>
2021-11-11 21:21:36 +0100

    faker implementation

--- src/test/java/tests/ContactFormTest.java
+++ src/test/java/tests/ContactFormTest.java
@@ -1,17 +1,19 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
 import pageObject.ContactUsPage;
 
 public class ContactFormTest {
 
     ContactUsPage contactUsPage = new ContactUsPage();
+    Faker faker = new Faker();
 
     @Test
     public void testContactFormWithBasicData(){
         contactUsPage
                 .goTo()
-                .sendFormWithBasicData("Webmaster","test@test.com","Lorem ipsum")
+                .sendFormWithBasicData("Webmaster","test@test.com",faker.lorem().paragraph())
                 .assertThatAlertSuccessContainsGivenText("Your message has been successfully sent to our team.");
     }
 }
